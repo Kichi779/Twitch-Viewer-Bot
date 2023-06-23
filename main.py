b5725e09e83b3024da9a5158e8e421c567f30378
Kichi779 <92625816+Kichi779@users.noreply.github.com>
2023-06-23 13:06:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -115,6 +115,7 @@ def main():
     chrome_options.add_argument('--disable-logging')
     chrome_options.add_argument('--log-level=3')
     chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument("--mute-audio")
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
