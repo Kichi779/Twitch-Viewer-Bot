1db72fa5d0895354500775a759c82f2fa5bae1c2
Kichi779 <92625816+Kichi779@users.noreply.github.com>
2024-07-02 12:48:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,59 +27,26 @@ def check_for_updates():
     except:
         return True
 
-def save_settings(twitch_username, set_160p):
+def save_settings(twitch_username):
     with open('settings.txt', 'w') as file:
         file.write(f"Twitch Username: {twitch_username}\n")
-        file.write(f"Set 160p: {set_160p}\n")
 
 def load_settings():
     try:
         with open('settings.txt', 'r') as file:
             lines = file.readlines()
-            if len(lines) >= 2:
+            if len(lines) >= 1:
                 twitch_username = lines[0].split(': ')[1].strip()
-                set_160p = lines[1].split(': ')[1].strip()
-                return twitch_username, set_160p
+                return twitch_username
     except:
         pass
-    return None, None
-
-
-
-
-def set_stream_quality(driver, quality):
-    if quality == "yes":
-        element_xpath = "//div[@data-a-target='player-overlay-click-handler']"
-
-        element = driver.find_element(By.XPATH, element_xpath)
-
-        actions = ActionChains(driver)
-
-        actions.move_to_element(element).perform()
-
-        settings_button = driver.find_element(By.XPATH, "//button[@aria-label='Settings']")
-        settings_button.click()
-
-        wait = WebDriverWait(driver, 10)
-        quality_option = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[text()='Quality']")))
-        quality_option.click()
-
-        time.sleep(15)
-
-        quality_levels = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'video-quality-option')]")))
-        
-        target_quality = "160p"
-        for level in quality_levels:
-            if target_quality in level.text:
-                level.click()
-                break
-
+    return None
 
 def main():
     if not check_for_updates():
         return
 
-    twitch_username, set_160p = load_settings()
+    twitch_username = load_settings()
 
 def print_announcement():
     try:
@@ -89,14 +56,12 @@ def print_announcement():
     except:
         print("Could not retrieve announcement from GitHub.\n")
 
-
-
 def main():
     if not check_for_updates():
         return
     print_announcement()
     
-    twitch_username, set_160p = load_settings()
+    twitch_username = load_settings()
 
     os.system(f"title Kichi779 - Twitch Viewer Bot @kichi#0779 ")
 
@@ -128,24 +93,21 @@ def main():
     proxy_url = selectRandom(proxy_servers)
 
     print(Colors.red, "Proxy servers are randomly selected every time")
-    if twitch_username is None or set_160p is None:
+    if twitch_username is None:
         
         twitch_username = input(Colorate.Vertical(Colors.green_to_blue, "Enter your channel name (e.g Kichi779): "))
-        set_160p = input(Colorate.Vertical(Colors.purple_to_red,"Do you want to set the stream quality to 160p? (yes/no): "))
 
-        save_settings(twitch_username, set_160p)
+        save_settings(twitch_username)
 
     else:
         use_settings = input(Colorate.Vertical(Colors.green_to_blue, "Do you want to use your saved settings? (yes/no): "))
         if use_settings.lower() == "no":
             
             twitch_username = input(Colorate.Vertical(Colors.green_to_blue, "Enter your channel name (e.g Kichi779): "))
-            set_160p = input(Colorate.Vertical(Colors.purple_to_red,"Do you want to set the stream quality to 160p? (yes/no): "))
 
-            save_settings(twitch_username, set_160p)
+            save_settings(twitch_username)
         
     proxy_count = int(input(Colorate.Vertical(Colors.cyan_to_blue, "How many proxy sites do you want to open? (Viewer to send)")))
-
 
     os.system("cls")
     print(Colorate.Vertical(Colors.green_to_cyan, Center.XCenter("""
@@ -165,7 +127,6 @@ def main():
     print('')
     print('')
     print(Colors.red, Center.XCenter("Viewers Send. Please don't hurry. If the viewers does not arrive, turn it off and on and do the same operations"))
-
 
     chrome_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
     driver_path = 'chromedriver.exe'
@@ -189,13 +150,10 @@ def main():
         driver.switch_to.window(driver.window_handles[-1])
         driver.get(random_proxy_url)
 
-
         text_box = driver.find_element(By.ID, 'url')
         text_box.send_keys(f'www.twitch.tv/{twitch_username}')
         text_box.send_keys(Keys.RETURN)
         time.sleep(10)
-
-    set_stream_quality(driver, set_160p)
 
     input(Colorate.Vertical(Colors.red_to_blue, "Viewers have all been sent. You can press enter to withdraw the views and the program will close."))
     driver.quit()
