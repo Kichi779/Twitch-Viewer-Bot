5a94f840001337b73e8fe784b65622a349bb18f3
Kichi779 <92625816+Kichi779@users.noreply.github.com>
2024-08-02 14:54:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import requests
 import warnings
-import random
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from colorama import Fore
 from pystyle import Center, Colors, Colorate
 import os
@@ -27,60 +23,10 @@ def check_for_updates():
     except:
         return True
 
-def save_settings(twitch_username, set_160p):
-    with open('settings.txt', 'w') as file:
-        file.write(f"Twitch Username: {twitch_username}\n")
-        file.write(f"Set 160p: {set_160p}\n")
-
-def load_settings():
-    try:
-        with open('settings.txt', 'r') as file:
-            lines = file.readlines()
-            if len(lines) >= 2:
-                twitch_username = lines[0].split(': ')[1].strip()
-                set_160p = lines[1].split(': ')[1].strip()
-                return twitch_username, set_160p
-    except:
-        pass
-    return None, None
-
-
-
-
-def set_stream_quality(driver, quality):
-    if quality == "yes":
-        element_xpath = "//div[@data-a-target='player-overlay-click-handler']"
-
-        element = driver.find_element(By.XPATH, element_xpath)
-
-        actions = ActionChains(driver)
-
-        actions.move_to_element(element).perform()
-
-        settings_button = driver.find_element(By.XPATH, "//button[@aria-label='Settings']")
-        settings_button.click()
-
-        wait = WebDriverWait(driver, 10)
-        quality_option = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[text()='Quality']")))
-        quality_option.click()
-
-        time.sleep(15)
-
-        quality_levels = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'video-quality-option')]")))
-        
-        target_quality = "160p"
-        for level in quality_levels:
-            if target_quality in level.text:
-                level.click()
-                break
-
 
 def main():
     if not check_for_updates():
         return
-
-    twitch_username, set_160p = load_settings()
-
 def print_announcement():
     try:
         r = requests.get("https://raw.githubusercontent.com/Kichi779/Twitch-Viewer-Bot/main/announcement.txt", headers={"Cache-Control": "no-cache"})
@@ -96,7 +42,6 @@ def main():
         return
     print_announcement()
     
-    twitch_username, set_160p = load_settings()
 
     os.system(f"title Kichi779 - Twitch Viewer Bot @kichi#0779 ")
 
@@ -112,7 +57,7 @@ def main():
                        ███   ▀█▀ █▀    ████████▀    ███    █▀    █▀   
                        ▀                                             
  Improvements can be made to the code. If you're getting an error, visit my discord.
-                             Discord discord.gg/fesaScZqpn    
+                             Discord discord.gg/AFV9m8UXuT    
                              Github  github.com/kichi779    """)))
     announcement = print_announcement()
     print("")
@@ -122,32 +67,26 @@ def main():
     print("")
     
 
-    proxy_servers = ['https://www.blockaway.net', 'https://www.croxyproxy.com', 'https://www.croxyproxy.rocks', 'https://www.croxy.network', 'https://www.croxy.org', 'https://www.youtubeunblocked.live', 'https://www.croxyproxy.net']
-    def selectRandom(proxy_servers):
-        return random.choice(proxy_servers)
+    proxy_servers = {
+        1: "https://www.blockaway.net",
+        2: "https://www.croxyproxy.com",
+        3: "https://www.croxyproxy.rocks",
+        4: "https://www.croxy.network",
+        5: "https://www.croxy.org",
+        6: "https://www.youtubeunblocked.live",
+        7: "https://www.croxyproxy.net",
+    }
 
-    proxy_url = selectRandom(proxy_servers)
+    # Selecting proxy server
+    print(Colors.green,"Proxy Server 1 Is Recommended")
+    print(Colorate.Vertical(Colors.green_to_blue,"Please select a proxy server(1,2,3..):"))
+    for i in range(1, 7):
+        print(Colorate.Vertical(Colors.red_to_blue,f"Proxy Server {i}"))
+    proxy_choice = int(input("> "))
+    proxy_url = proxy_servers.get(proxy_choice)
 
-    print(Colors.red, "Proxy servers are randomly selected every time")
-    if twitch_username is None or set_160p is None:
-        
-        twitch_username = input(Colorate.Vertical(Colors.green_to_blue, "Enter your channel name (e.g Kichi779): "))
-        set_160p = input(Colorate.Vertical(Colors.purple_to_red,"Do you want to set the stream quality to 160p? (yes/no): "))
-
-        save_settings(twitch_username, set_160p)
-
-    else:
-        use_settings = input(Colorate.Vertical(Colors.green_to_blue, "Do you want to use your saved settings? (yes/no): "))
-        if use_settings.lower() == "no":
-            
-            twitch_username = input(Colorate.Vertical(Colors.green_to_blue, "Enter your channel name (e.g Kichi779): "))
-            set_160p = input(Colorate.Vertical(Colors.purple_to_red,"Do you want to set the stream quality to 160p? (yes/no): "))
-
-            save_settings(twitch_username, set_160p)
-        
+    twitch_username = input(Colorate.Vertical(Colors.green_to_blue, "Enter your channel name (e.g Kichi779): "))
     proxy_count = int(input(Colorate.Vertical(Colors.cyan_to_blue, "How many proxy sites do you want to open? (Viewer to send)")))
-
-
     os.system("cls")
     print(Colorate.Vertical(Colors.green_to_cyan, Center.XCenter("""
            
@@ -161,7 +100,7 @@ def main():
                        ███   ▀█▀ █▀    ████████▀    ███    █▀    █▀   
                        ▀                                             
  Improvements can be made to the code. If you're getting an error, visit my discord.
-                             Discord discord.gg/UkSJP8RUxc    
+                             Discord discord.gg/AFV9m8UXuT    
                              Github  github.com/kichi779    """)))
     print('')
     print('')
@@ -174,10 +113,9 @@ def main():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('--disable-logging')
-    chrome_options.add_argument("--lang=en")
-
     chrome_options.add_argument('--log-level=3')
     chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument("--mute-audio")
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
@@ -185,21 +123,17 @@ def main():
     driver.get(proxy_url)
 
     for i in range(proxy_count):
-        random_proxy_url = selectRandom(proxy_servers)  # Select a random proxy server for this tab
-        driver.execute_script("window.open('" + random_proxy_url + "')")
+        driver.execute_script("window.open('" + proxy_url + "')")
         driver.switch_to.window(driver.window_handles[-1])
-        driver.get(random_proxy_url)
-
+        driver.get(proxy_url)
 
         text_box = driver.find_element(By.ID, 'url')
         text_box.send_keys(f'www.twitch.tv/{twitch_username}')
         text_box.send_keys(Keys.RETURN)
-        time.sleep(10)
-
-    set_stream_quality(driver, set_160p)
 
     input(Colorate.Vertical(Colors.red_to_blue, "Viewers have all been sent. You can press enter to withdraw the views and the program will close."))
     driver.quit()
+
 
 if __name__ == '__main__':
     main()
