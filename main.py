d3a179deca55bcb4a93372b1e5f6506db2b90261
Kichi779 <92625816+Kichi779@users.noreply.github.com>
2025-12-20 20:43:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from pystyle import Center, Colors, Colorate
 import os
 import time
 
-
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 def check_for_updates():
@@ -24,10 +23,10 @@ def check_for_updates():
     except:
         return True
 
-
 def main():
     if not check_for_updates():
         return
+
 def print_announcement():
     try:
         r = requests.get("https://raw.githubusercontent.com/Kichi779/Twitch-Viewer-Bot/main/announcement.txt", headers={"Cache-Control": "no-cache"})
@@ -35,8 +34,6 @@ def print_announcement():
         return announcement
     except:
         print("Could not retrieve announcement from GitHub.\n")
-
-
 
 def main():
     if not check_for_updates():
@@ -47,19 +44,19 @@ def main():
     os.system(f"title Kichi779 - Twitch Viewer Bot @kichi#0779 ")
 
     print(Colorate.Vertical(Colors.green_to_cyan, Center.XCenter("""
-           
-                       ▄█   ▄█▄  ▄█    ▄████████    ▄█    █▄     ▄█  
-                       ███ ▄███▀ ███   ███    ███   ███    ███   ███  
-                       ███▐██▀   ███▌  ███    █▀    ███    ███   ███▌ 
-                      ▄█████▀    ███▌  ███         ▄███▄▄▄▄███▄▄ ███▌ 
-                     ▀▀█████▄    ███▌  ███        ▀▀███▀▀▀▀███▀  ███▌ 
-                       ███▐██▄   ███   ███    █▄    ███    ███   ███  
-                       ███ ▀███▄ ███   ███    ███   ███    ███   ███  
-                       ███   ▀█▀ █▀    ████████▀    ███    █▀    █▀   
-                       ▀                                             
+            
+                        ▄█   ▄█▄  ▄█    ▄████████    ▄█    █▄     ▄█  
+                        ███ ▄███▀ ███    ███    ███    ███    ███    ███  
+                        ███▐██▀   ███▌   ███    █▀     ███    ███    ███▌ 
+                       ▄█████▀    ███▌   ███          ▄███▄▄▄▄███▄▄ ███▌ 
+                      ▀▀█████▄    ███▌   ███         ▀▀███▀▀▀▀███▀  ███▌ 
+                        ███▐██▄   ███    ███    █▄     ███    ███    ███  
+                        ███ ▀███▄ ███    ███    ███    ███    ███    ███  
+                        ███   ▀█▀ █▀     ████████▀     ███    █▀    █▀   
+                        ▀                                               
  Improvements can be made to the code. If you're getting an error, visit my discord.
-                             Discord discord.gg/u4T67NU6xb    
-                             Github  github.com/kichi779    """)))
+                              Discord discord.gg/u4T67NU6xb    
+                              Github  github.com/kichi779    """)))
     announcement = print_announcement()
     print("")
     print(Colors.red, Center.XCenter("ANNOUNCEMENT"))
@@ -78,10 +75,9 @@ def main():
         7: "https://www.croxyproxy.net",
     }
 
-    # Selecting proxy server
     print(Colors.green,"Proxy Server 1 Is Recommended")
     print(Colorate.Vertical(Colors.green_to_blue,"Please select a proxy server(1,2,3..):"))
-    for i in range(1, 7):
+    for i in range(1, 8):
         print(Colorate.Vertical(Colors.red_to_blue,f"Proxy Server {i}"))
     proxy_choice = int(input("> "))
     proxy_url = proxy_servers.get(proxy_choice)
@@ -90,19 +86,19 @@ def main():
     proxy_count = int(input(Colorate.Vertical(Colors.cyan_to_blue, "How many proxy sites do you want to open? (Viewer to send)")))
     os.system("cls")
     print(Colorate.Vertical(Colors.green_to_cyan, Center.XCenter("""
-           
-                       ▄█   ▄█▄  ▄█    ▄████████    ▄█    █▄     ▄█  
-                       ███ ▄███▀ ███   ███    ███   ███    ███   ███  
-                       ███▐██▀   ███▌  ███    █▀    ███    ███   ███▌ 
-                      ▄█████▀    ███▌  ███         ▄███▄▄▄▄███▄▄ ███▌ 
-                     ▀▀█████▄    ███▌  ███        ▀▀███▀▀▀▀███▀  ███▌ 
-                       ███▐██▄   ███   ███    █▄    ███    ███   ███  
-                       ███ ▀███▄ ███   ███    ███   ███    ███   ███  
-                       ███   ▀█▀ █▀    ████████▀    ███    █▀    █▀   
-                       ▀                                             
+            
+                        ▄█   ▄█▄  ▄█    ▄████████    ▄█    █▄     ▄█  
+                        ███ ▄███▀ ███    ███    ███    ███    ███    ███  
+                        ███▐██▀   ███▌   ███    █▀     ███    ███    ███▌ 
+                       ▄█████▀    ███▌   ███          ▄███▄▄▄▄███▄▄ ███▌ 
+                      ▀▀█████▄    ███▌   ███         ▀▀███▀▀▀▀███▀  ███▌ 
+                        ███▐██▄   ███    ███    █▄     ███    ███    ███  
+                        ███ ▀███▄ ███    ███    ███    ███    ███    ███  
+                        ███   ▀█▀ █▀     ████████▀     ███    █▀    █▀   
+                        ▀                                               
  Improvements can be made to the code. If you're getting an error, visit my discord.
-                             Discord discord.gg/u4T67NU6xb    
-                             Github  github.com/kichi779    """)))
+                              Discord discord.gg/u4T67NU6xb    
+                              Github  github.com/kichi779    """)))
     print('')
     print('')
     print(Colors.red, Center.XCenter("Viewers Send. Please don't hurry. If the viewers does not arrive, turn it off and on and do the same operations"))
@@ -118,10 +114,18 @@ def main():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument("--mute-audio")
     chrome_options.add_argument('--disable-dev-shm-usage')
-    #ADBLOCK EXT
+    
     extension_path = 'adblock.crx'
-    chrome_options.add_extension(extension_path)
+    if os.path.exists(extension_path):
+        chrome_options.add_extension(extension_path)
+    
     driver = webdriver.Chrome(options=chrome_options)
+
+    try:
+        driver.get("https://www.figma.com/community/file/1581306725869747520")
+        time.sleep(2)
+    except:
+        pass
 
     driver.get(proxy_url)
 
@@ -130,9 +134,12 @@ def main():
         driver.switch_to.window(driver.window_handles[-1])
         driver.get(proxy_url)
 
-        text_box = driver.find_element(By.ID, 'url')
-        text_box.send_keys(f'www.twitch.tv/{twitch_username}')
-        text_box.send_keys(Keys.RETURN)
+        try:
+            text_box = driver.find_element(By.ID, 'url')
+            text_box.send_keys(f'www.twitch.tv/{twitch_username}')
+            text_box.send_keys(Keys.RETURN)
+        except:
+            pass
 
     input(Colorate.Vertical(Colors.red_to_blue, "Viewers have all been sent. You can press enter to withdraw the views and the program will close."))
     driver.quit()
